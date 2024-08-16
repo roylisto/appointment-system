4484bf09b9795bb145ec9e705ecc42ff7ef73093
Roy Pradana <33563774+roylisto@users.noreply.github.com>
2024-08-16 14:22:04 +0700

    feat: setup database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [__dirname + '/**/*.entity.{js,ts}'],
+    synchronize: true, // set to false in production
+  })
+],
   controllers: [AppController],
   providers: [AppService],
 })
